34f9949a6eb169574851b8d003173628cb0d800e
Michael Chirico <MichaelChirico4@gmail.com>
2017-03-03 15:42:27 -0500

    who needs working code when you can have working error messages!

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,17 +1,21 @@
 # for setting options & assigning any packing internal variables
 
-# .onLoad <- function(libname, pkgname) {
-#   opts = list("texr.labelcount" = 0L,
-#               "texr.params" = object_to_text(.global$default_params),
-#               "texr.warn.rotating" = TRUE,
-#               "texr.warn.rotating.toggle" = TRUE)
-#   for (ii in names(opts)) {
-#     eval(parse(text = paste0("options(", ii, "=", opts[[ii]], ")")))
-#   }
-#   
-#   #have to declare these AFTER the above runs,
-#   #  but also can't declare .global from within
-#   #  the scope of this function
-#   .global$label_count <- getOption("texr.labelcount") 
-#   .global$warn_rotate <- getOption("texr.warn.rotating")
-# }
+.onLoad <- function(libname, pkgname) {
+  if (system('which vw')) {
+    warning(paste('vw command line utility not found -- train/test',
+                  'functionality will not work. I/O tools still available.'))
+  }
+  # opts = list("texr.labelcount" = 0L,
+  #             "texr.params" = object_to_text(.global$default_params),
+  #             "texr.warn.rotating" = TRUE,
+  #             "texr.warn.rotating.toggle" = TRUE)
+  # for (ii in names(opts)) {
+  #   eval(parse(text = paste0("options(", ii, "=", opts[[ii]], ")")))
+  # }
+  # 
+  # #have to declare these AFTER the above runs,
+  # #  but also can't declare .global from within
+  # #  the scope of this function
+  # .global$label_count <- getOption("texr.labelcount")
+  # .global$warn_rotate <- getOption("texr.warn.rotating")
+}
